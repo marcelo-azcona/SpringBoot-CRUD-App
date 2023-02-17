6b7ef6088d76f289b9d1751533c43613c9e315f6
marcelo-azcona <marceloazcona86@gmail.com>
2023-02-17 19:57:25 -0300

    Change all package`s name

--- src/main/java/com/cruddatabaseapp/app/CrudDatabaseAppApplication.java
+++ src/main/java/com/cruddatabaseapp/app/CrudDatabaseAppApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.cruddatabaseapp;
+package com.cruddatabaseapp.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
