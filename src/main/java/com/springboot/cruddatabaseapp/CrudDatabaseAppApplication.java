5b454f4a56c4842efa849839648292b309331a60
marcelo-azcona <marceloazcona86@gmail.com>
2023-02-17 18:52:30 -0300

    Uploading springboot initial files

---
+++ src/main/java/com/springboot/cruddatabaseapp/CrudDatabaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.cruddatabaseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDatabaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDatabaseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/cruddatabaseapp/CrudDatabaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cruddatabaseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDatabaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
