ee82631cad1e6818bd65fdaa869f85d729570a6a
marcelo-azcona <marceloazcona86@gmail.com>
2023-02-25 20:07:48 -0300

    Updating CRUD methods

--- src/main/java/com/cruddatabaseapp/rest/EmployeeRestController.java
+++ src/main/java/com/cruddatabaseapp/rest/EmployeeRestController.java
@@ -3,8 +3,12 @@ package com.cruddatabaseapp.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,10 +40,45 @@ public class EmployeeRestController {
 		
 		Employee theEmployee = employeeService.findById(employeeId);
 		
-		if(theEmployee == null) {
+		if (theEmployee == null) {
 			throw new RuntimeException("Employee not found " + employeeId);
 		}
 		
 		return theEmployee;
+	}
+	
+	// Add a new employee - RequestBody is for the rquest body data transformation: JSON data --> Object data
+	@PostMapping("/employee/{employeeId}")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		
+		// to force a save of a new item, instead of an update. The DB will asign the id.
+		theEmployee.setId(0);
+		
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
+	}
+	
+	// Update employee
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+			
+		employeeService.save(theEmployee);
+			
+		return theEmployee;
+	}
+	
+	@DeleteMapping("/employee/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		
+		Employee tempEmployee = employeeService.findById(employeeId);
+			
+		if (tempEmployee == null) {
+			throw new RuntimeException("Employee id not found " + employeeId);
+		}
+		
+		employeeService.deleteById(employeeId);
+		
+		return "Deleted employee with the id: " + employeeId;
 	}
 }
