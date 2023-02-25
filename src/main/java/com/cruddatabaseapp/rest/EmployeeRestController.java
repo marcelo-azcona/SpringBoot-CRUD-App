87f476057b5b1918f5291ce61d2d77c059bb45f6
marcelo-azcona <marceloazcona86@gmail.com>
2023-02-25 19:28:52 -0300

    Create a method to read a single employee

--- src/main/java/com/cruddatabaseapp/rest/EmployeeRestController.java
+++ src/main/java/com/cruddatabaseapp/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +19,27 @@ public class EmployeeRestController {
 	
 	@Autowired
 	public EmployeeRestController(EmployeeService theEmployeeService) {
+		
 		this.employeeService = theEmployeeService;
 	}
 	
 	// expose the "/employees" endpoint and return list of employee
 	@GetMapping("/employees")
 	public List<Employee> findAll(){
+		
 		return employeeService.findAll();
+	}
+	
+	// read a single employee by the Id
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId){
+		
+		Employee theEmployee = employeeService.findById(employeeId);
+		
+		if(theEmployee == null) {
+			throw new RuntimeException("Employee not found " + employeeId);
+		}
+		
+		return theEmployee;
 	}
 }
