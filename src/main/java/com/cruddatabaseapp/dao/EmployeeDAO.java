0585e51a2e7476c0fdec6c68fcbd9aab69e266d3
marcelo-azcona <marceloazcona86@gmail.com>
2023-02-25 18:44:55 -0300

    Update the DAO with all CRUD methods

--- src/main/java/com/cruddatabaseapp/dao/EmployeeDAO.java
+++ src/main/java/com/cruddatabaseapp/dao/EmployeeDAO.java
@@ -7,5 +7,10 @@ import com.cruddatabaseapp.entity.Employee;
 public interface EmployeeDAO {
 
 	public List<Employee> findAll();
+	
 	public Employee findById(int id);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int id); 
 }
--- src/main/java/com/cruddatabaseapp/dao/EmployeeDAOImplementation.java
+++ src/main/java/com/cruddatabaseapp/dao/EmployeeDAOImplementation.java
@@ -25,13 +25,13 @@ public class EmployeeDAOImplementation implements EmployeeDAO {
 	}
 	
 	@Override
-	@Transactional
+	// @Transactional
 	public List<Employee> findAll() {
 
 		// get the current hibernate session
 		Session currentSession = entityManager.unwrap(Session.class);
 		
-		// create a query
+		// create a query for the DB
 		Query<Employee> theQuery = currentSession.createQuery("from Employee", Employee.class);
 		
 		// execute the query and get the result list
@@ -39,6 +39,46 @@ public class EmployeeDAOImplementation implements EmployeeDAO {
 		
 		// return the results
 		return employees;
+	}
+	
+	@Override
+	// @Transactional
+	public Employee findById(int id) {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// get the employee from the DB
+		Employee theEmployee = currentSession.get(Employee.class, id);
+		
+		return theEmployee;
+	}
+
+	@Override
+	// @Transactional
+	public void save(Employee theEmployee) {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+				
+		// save the employee
+		currentSession.saveOrUpdate(theEmployee);
+	}
+
+	@Override
+	// @Transactional
+	public void deleteById(int id) {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+				
+		// delete object with primary key
+		Query<Employee> theQuery = currentSession.createQuery(
+										"delete from Employee where id=:employeeId");
+		
+		theQuery.setParameter("employeeId", id);
+		
+		theQuery.executeUpdate();
 	}
 
 }
